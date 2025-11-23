5b69f8e9a510f9ae0e439a30c2c780efc21c62af
Pierre-Henri Dezanneau <ph.dezanneau@gmail.com>
2025-11-23 16:39:44 +0100

    Fix method to test if an url is reachable or not

--- src/main/java/org/phdezann/cn/support/IOUtils.java
+++ src/main/java/org/phdezann/cn/support/IOUtils.java
@@ -1,16 +1,26 @@
 package org.phdezann.cn.support;
 
-import java.net.URL;
+import java.net.HttpURLConnection;
+import java.net.URI;
 
 public class IOUtils {
 
     public static boolean isUrlReachable(String url) {
+        HttpURLConnection connection = null;
         try {
-            var connection = new URL(url).openConnection();
+            var u = new URI(url).toURL();
+            connection = (HttpURLConnection) u.openConnection();
+            connection.setRequestMethod("HEAD");
+            connection.setConnectTimeout(5_000);
+            connection.setReadTimeout(5_000);
             connection.connect();
-            return true;
+            return connection.getResponseCode() < 500;
         } catch (Exception e) {
             return false;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
         }
     }
 }
